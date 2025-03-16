a40e151ec6e07436b957a38c6e99e2b316752ba7
Diana <narduak.123@gmail.com>
2025-03-16 11:52:52 +0200

    Вдосконалення бургерного меню

--- burger/burger.js
+++ burger/burger.js
@@ -2,3 +2,19 @@ function toggleMenu() {
     const nav = document.getElementById('burgerNav');
     nav.classList.toggle('active');
 }
+
+function checkScreenSize() {
+    const burgerMenu = document.querySelector('.burger-menu');
+    const bottomNav = document.querySelector('.bottom-nav');
+
+    if (window.innerWidth <= 768) {
+        burgerMenu.style.display = 'block';
+        bottomNav.style.display = 'none';
+    } else {
+        burgerMenu.style.display = 'none';
+        bottomNav.style.display = 'block';
+    }
+}
+
+window.addEventListener('load', checkScreenSize);
+window.addEventListener('resize', checkScreenSize);
